a1d8b3fafc358b9c79572b5cfada8efaefd86e6f
gpt <gabriel.pulidodetorres@gmail.com>
2012-02-11 19:31:58 +0100

    Clean libraries

--- src/es/gpulido/freedomotic/ui/RoomsFragment.java
+++ src/es/gpulido/freedomotic/ui/RoomsFragment.java
@@ -16,6 +16,10 @@ import com.viewpagerindicator.TitlePageIndicator;
 import com.viewpagerindicator.TitleProvider;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
